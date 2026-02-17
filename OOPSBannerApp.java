1f240c8cbb2f1f6f6c89ea69bc912be8c41509ed
im-shourya <parasharshourya07@gmail.com>
2026-02-17 11:08:20 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,10 @@
+/**
+ * UC1
+ * * @author Shourya Parashar
+ * @version 1.0.0 (17 Feb 2026)
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
