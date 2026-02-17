d9c48f43dde47d154d2880bd351b107677c78aba
Shourya <parasharshourya07@gmail.com>
2026-02-17 10:29:45 +0530

    Inital commit

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,3 +1,8 @@
+/**
+ * UC1
+ * * @author Shourya Parashar
+ * @version 1.0.0 (17 Feb 2026)
+ */
 public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println("OOPS");
